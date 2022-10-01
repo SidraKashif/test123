60156fd9e31c16423171929dd531ba03a0cfdcde
Sidra <sidrakas.khan21@gmail.com>
2022-10-01 11:53:01 -0500

    Done

---
+++ assignment1/src/assignment1/Assignment2.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+c=a;
+a=b;
+b=c;
+System.out.println("a="+a);
+System.out.println("b="+b);
+	}
+
+}
---
+++ assignment1/src/assignment1/assignment1.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		String s="Hello";
+		String a="Sidra";
+		System.out.println(s +" "+ a);
+
+	}
+
+}
